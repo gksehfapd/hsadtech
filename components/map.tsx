7f815225bc59c399ab16156d2ddc3bfb67769ddf
gksehfapd <1ehfapd@gmail.com>
2024-04-28 17:26:24 +0900

    feat: kakaomap

---
+++ components/map.tsx
@@ -0,0 +1,47 @@
+import { Map } from 'react-kakao-maps-sdk'
+import { useEffect, useState } from 'react'
+
+declare global {
+	interface Window {
+		kakao: any
+	}
+}
+
+const KakaoMap = () => {
+	useEffect(() => {
+		const kakaoMapScript = document.createElement('script')
+		kakaoMapScript.async = false
+		kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=700d399006256f95732f06b19c046ba5&autoload=false`
+		document.head.appendChild(kakaoMapScript)
+
+		const onLoadKakaoAPI = () => {
+			window.kakao.maps.load(() => {
+				var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+					mapOption = {
+						center: new kakao.maps.LatLng(37.5458559401876, 127.047977996156), // 지도의 중심좌표
+						level: 3 // 지도의 확대 레벨
+					}
+
+				//@ts-ignore
+				var map = new kakao.maps.Map(mapContainer, mapOption) // 지도를 생성합니다
+
+				// 마커가 표시될 위치입니다
+				var markerPosition = new kakao.maps.LatLng(37.5458559401876, 127.047977996156)
+
+				// 마커를 생성합니다
+				var marker = new kakao.maps.Marker({
+					position: markerPosition
+				})
+
+				// 마커가 지도 위에 표시되도록 설정합니다
+				marker.setMap(map)
+			})
+		}
+
+		kakaoMapScript.addEventListener('load', onLoadKakaoAPI)
+	}, [])
+
+	return <div id="map" style={{ width: '100%', height: '100%' }}></div>
+}
+
+export default KakaoMap
--- components/pageComponents/contactusComponent.tsx
+++ components/pageComponents/contactusComponent.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form'
+import KakaoMap from '../map'
 
 const ContactusComponent = () => {
 	const { register, handleSubmit } = useForm()
@@ -6,10 +7,11 @@ const ContactusComponent = () => {
 	const onSubmit = (data: any) => {
 		console.log(data)
 	}
+
 	return (
 		<div className="flex flex-col pb-8">
 			<span className="text-5xl text-purple-900 pb-6">Contact us</span>
-			<div className="flex flex-col text-xl">
+			<div className="flex flex-col text-xl pb-20">
 				<span className="mb-6">
 					HS Adtech의 제휴 및 퍼포먼스 등 통합 서비스를 통한 성공적인 마케팅과 최고의
 					성과를 달성해 보세요!
@@ -57,6 +59,36 @@ const ContactusComponent = () => {
 						className="bg-slate-300 w-40 h-16 rounded-lg mt-8"
 					/>
 				</form>
+			</div>
+
+			<div className="w-full h-96 flex  max-md:flex-col">
+				<div className="w-4/5 h-96 max-md:w-full max-md:h-72">
+					<KakaoMap />
+				</div>
+				<div className="w-1/5 p-6 bg-zinc-700 justify-center items-center text-white text-base flex max-md:h-32 max-md:w-full max-md:p-0">
+					<ul className="float-none flex flex-col items-center h-full justify-around py-10 max-md:py-2 max-md:text-sm max-md:items-start">
+						<li className="py-1 max-md:py-0">
+							<span className="flex items-center">
+								<span className="material-icons mr-2">near_me</span>
+								서울특별시 성동구 상원1길 25 SOL빌딩 4층
+							</span>
+						</li>
+						<li className="py-1">
+							<span className="flex items-center">
+								<span className="material-icons mr-2">phone</span>
+								<div className="flex flex-col">
+									<span>TEL : 02-462-2384</span> <span> FAX : 0504-001-2384</span>
+								</div>
+							</span>
+						</li>
+						<li className="py-1">
+							<span className="flex items-center">
+								<span className="material-icons mr-2">mail</span>{' '}
+								hsadtech@hsadtech.com
+							</span>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	)
