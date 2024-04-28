13ee12852c2ea252b963f8ec281866473d334d7a
gksehfapd <1ehfapd@gmail.com>
2024-04-28 21:00:53 +0900

    fix: vercel kakaomap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,7 @@ export default function Document() {
 			></link>
 			<script
 				type="text/javascript"
-				src="//dapi.kakao.com/v2/maps/sdk.js?appkey=발급받은 APP KEY를 넣으시면 됩니다."
+				src="//dapi.kakao.com/v2/maps/sdk.js?appkey=700d399006256f95732f06b19c046ba5"
 			></script>
 			<body>
 				<Main />
